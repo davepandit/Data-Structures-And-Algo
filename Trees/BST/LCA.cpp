adb2998adf5821101049cdb32fabe4d352db1439
davepandit <debajyotipandit8@gmail.com>
2025-03-14 14:03:31 +0530

    LCA and how to make a bt from preorder and postorder traversal

---
+++ Trees/BST/LCA.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+ class Solution {
+    public:
+        TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
+            TreeNode* node = root;
+            while(node){
+                if(node->val < p->val && node->val < q->val){
+                    // move to the right
+                    node = node->right;
+                }
+                else if(node->val > p->val && node->val > q->val){
+                    // move to the left
+                    node = node->left;
+                }
+                else{
+                    return node;
+                }
+            }
+    
+            return root;
+        }
+        TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+            if(root == NULL){
+                return root;
+            }
+    
+            TreeNode* ans = solve(root, p, q);
+    
+            return ans;
+        }
+    };
